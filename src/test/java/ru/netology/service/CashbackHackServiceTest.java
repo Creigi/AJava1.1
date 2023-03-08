cc5efe1c94ce8fb917191e6f7ed01eb9dd12bad4
Aleksandr Baronov <creigi@yandex.ru>
2023-03-08 16:40:32 +0400

    branch junit4-platform added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,8 @@
 package ru.netology.service;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -36,5 +39,37 @@ public class CashbackHackServiceTest {
         int actual = service.remain(445);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainZeroJ5() {
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainOneJ5() {
+        int expected = 1;
+        int actual = service.remain(999);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainOneThousandJ5() {
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainSomeJ5() {
+        int expected = 555;
+        int actual = service.remain(445);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
